6a3b05af9b5c12cc15e064baf622c816defa57d2
sunteya <sunteya@gmail.com>
2013-07-13 00:25:47 +0800

    create new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PreludeClient::Application.config.secret_key_base = 'b66ee059cfedde9cb17f6433790fae9c53aeb104743579c754a6caf2ac632b7b278f77a3227598e5de30d5c07b35f731ee8c9f87640ea7663489870faaa5b64d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PreludeClient::Application.config.session_store :cookie_store, key: '_prelude-client_session'
