adebacba182fde2a0fe9282aea6a5d7cf9885844
sunteya <sunteya@gmail.com>
2013-07-13 00:46:52 +0800

    deploy script force chown to www-data

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require "capsum/typical"
 
 set :application, "prelude-client"
-# set :repository, ".git"
+set :repository, ".git"
 
 set :shared, %w{
   config/database.yml
--- config/deploy/p.wido.me.rb
+++ config/deploy/p.wido.me.rb
@@ -4,3 +4,11 @@ set :rails_env, "production"
 set :user, "root"
 server "p.wido.me", :app, :web, :db #, whenever: true, primary: true
 default_environment["http_proxy"] = default_environment["https_proxy"] = "http://localhost:8118"
+
+
+task :reset_permissions do
+  run "chown -R www-data:www-data '#{deploy_to}'"
+end
+
+after "deploy:setup", 'reset_permissions'
+after "deploy:update", 'reset_permissions'
