8ab5f2ca28428126ee72606fe232e1693d08f2f2
sunteya <sunteya@gmail.com>
2013-07-15 22:06:57 +0800

    fix upcode type

--- app/workers/sync_all_users_job.rb
+++ app/workers/sync_all_users_job.rb
@@ -4,7 +4,7 @@ class SyncAllUsersJob
   def perform
     users_json = Client.instance.users
     users_json.each do |json|
-      user = User.where(upcode: json['id']).first_or_initialize
+      user = User.where(upcode: json['id'].to_s).first_or_initialize
       user.email = json['email']
       user.revision = json['lock_version']
       user.binding_port = json['binding_port']
@@ -12,7 +12,7 @@ class SyncAllUsersJob
       user.save!
     end
 
-    deleted_users = User.without_deleted.where.not(upcode: users_json.map { |m| m['id'] })
+    deleted_users = User.without_deleted.where.not(upcode: users_json.map { |m| m['id'].to_s })
     deleted_users.each { |u| u.update(deleted_at: Time.now) }
   end
 end
