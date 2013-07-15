7bfa1f9c33ad0089ea57c7407925d3e088765275
sunteya <sunteya@gmail.com>
2013-07-15 12:14:12 +0800

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                       :integer          not null, primary key
+#  email                    :string(255)
+#  upcode                   :string(255)
+#  revision                 :integer
+#  binding_port             :integer
+#  transfer_remaining       :integer          default(0)
+#  freeze_transfer          :integer          default(0)
+#  local_transfer_remaining :integer          default(0)
+#  deleted_at               :datetime
+#  created_at               :datetime
+#  updated_at               :datetime
+#
+
+class User < ActiveRecord::Base
+  before_save :build_local_transfer_remaining
+
+  scope :available, -> { where("deleted_at NOT NULL").where("local_transfer_remaining > ?", 0) }
+
+  def build_local_transfer_remaining
+    self.local_transfer_remaining = self.transfer_remaining - self.freeze_transfer
+  end
+end
