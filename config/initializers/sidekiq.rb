4785dcd89c2aeba03cf04cb29f52e7fbe330c86a
sunteya <sunteya@gmail.com>
2013-07-15 21:52:02 +0800

    change sidekiq namespace

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :namespace => 'prelude-client' }
+  config.redis = { :namespace => 'prelude' }
   config.poll_interval = 1
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :namespace => 'prelude-client' }
+  config.redis = { :namespace => 'prelude' }
 end
