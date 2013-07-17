060dd7e323405d49b59262148b9cf2041f7271b2
sunteya <sunteya@gmail.com>
2013-07-17 15:16:57 +0800

    grant action

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,12 @@
+class MainController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :grant
+
+  def grant
+    if request.post? && params[:token] == Project.settings.grant_word
+      FileUtils.touch Rails.root.join("allow/#{request.ip}")
+      return render 'success', layout: false
+    end
+    
+    render 'input', layout: false
+  end
+end
