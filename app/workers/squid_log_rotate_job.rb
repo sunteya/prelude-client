d7337f2869c8895f85e6b6bfdfd3b6717de74dd0
sunteya <sunteya@gmail.com>
2013-07-19 09:24:31 +0800

    squid log rotate job

---
+++ app/workers/squid_log_rotate_job.rb
@@ -0,0 +1,20 @@
+class SquidLogRotateJob
+  include Sidekiq::Worker
+
+  def perform
+    squid_log_path = Project.settings.squid_log_path
+    if !File.exist?(squid_log_path)
+      begin
+        Cocaine::CommandLine.new(Project.settings.squid_bin, "-k rotate").run
+      rescue Cocaine::ExitStatusError => e
+        return # squid not running, skip
+      end
+    end
+
+    sleep 5
+    filename = File.basename(squid_log_path)
+    timestamp = Time.now.strftime("%Y%m%dT%H%M%S")
+    new_filename = "#{filename}-#{timestamp}"
+    SquidLogAnalysisJob.perform_async(new_filename)
+  end
+end
