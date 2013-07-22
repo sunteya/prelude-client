2ac0e3433921927404663ca49bdc1093063ff427
sunteya <sunteya@gmail.com>
2013-07-22 13:58:34 +0800

    fix sync_all_users_job ActiveRecord::StaleObjectError exception

--- app/workers/sync_all_users_job.rb
+++ app/workers/sync_all_users_job.rb
@@ -6,11 +6,16 @@ class SyncAllUsersJob
     users_json = Client.instance.users!['users']
     users_json.each do |json|
       user = User.where(upcode: json['id'].to_s).first_or_initialize
-      user.email = json['email']
-      user.binding_port = json['binding_port']
-      user.transfer_remaining = json['transfer_remaining']
-      user.deleted_at = false
-      user.save!
+      begin
+        user.email = json['email']
+        user.binding_port = json['binding_port']
+        user.transfer_remaining = json['transfer_remaining']
+        user.deleted_at = false
+        user.save!
+      rescue ActiveRecord::StaleObjectError
+        user.reload
+        retry
+      end
     end
 
     deleted_users = User.without_deleted.where.not(upcode: users_json.map { |m| m['id'].to_s })
