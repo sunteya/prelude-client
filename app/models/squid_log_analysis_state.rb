7fa7af40e8f13e608d41380a47691c832537f114
sunteya <sunteya@gmail.com>
2013-07-22 10:42:14 +0800

    cleanup log file on analysis state deleted

--- app/models/squid_log_analysis_state.rb
+++ app/models/squid_log_analysis_state.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: squid_log_analysis_states
+#
+#  id            :integer          not null, primary key
+#  filename      :string(255)
+#  log_file_path :string(1000)
+#  position      :integer          default(0)
+#  created_at    :datetime
+#  updated_at    :datetime
+#
+
 class SquidLogAnalysisState < ActiveRecord::Base
   validates :filename, presence: true
+  after_destroy :remove_log_file
+
+  def remove_log_file
+    FileUtils.rm(log_file_path) if File.exists?(log_file_path)
+  end
 end
